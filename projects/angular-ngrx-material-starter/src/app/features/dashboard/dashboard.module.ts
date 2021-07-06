a71947ef94558d37965138eb1f5e2d8fafd458d1
Emilija Joskovska <emilijaj@iborn.net>
2021-07-06 11:59:17 +0200

    1) change color 2) alter reset pass screen (should be the same in login page) 3) altered dashboard screen, needs few small changes

--- projects/angular-ngrx-material-starter/src/app/features/dashboard/dashboard.module.ts
+++ projects/angular-ngrx-material-starter/src/app/features/dashboard/dashboard.module.ts
@@ -4,14 +4,11 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from '../../shared/shared.module';
-
+import { DashboardItemComponent } from './dashboard-item/dashboard-item.component';
+import { NeedItemComponent } from './need-item/need-item.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    SharedModule
-  ]
+  declarations: [DashboardComponent, DashboardItemComponent, NeedItemComponent],
+  imports: [CommonModule, DashboardRoutingModule, SharedModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- projects/angular-ngrx-material-starter/src/app/features/dashboard/dashboard/dashboard.component.ts
+++ projects/angular-ngrx-material-starter/src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -23,38 +28,48 @@ import { DashboardService } from '../dashboard.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit {
-  types$: BehaviorSubject<FrequencyType[]> = new BehaviorSubject<FrequencyType[]>(null);
+  types$: BehaviorSubject<FrequencyType[]> = new BehaviorSubject<
+    FrequencyType[]
+  >(null);
   plants$: BehaviorSubject<Plant[]> = new BehaviorSubject<Plant[]>(null);
   months = {
-    1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-    7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
   };
-  displayedColumns: string[] = ['needId', 'monthFrom', 'monthTo', 'quantity', 'frequency', 'type'];
+  displayedColumns: string[] = [
+    'needId',
+    'monthFrom',
+    'monthTo',
+    'quantity',
+    'frequency',
+    'type'
+  ];
   selectedType: number = 1;
-  user: any;
-  curDate: any;
+
   model: Action;
-  AddAction: FormGroup;
   actionNeed: boolean = false;
 
   constructor(
     private dashboardService: DashboardService,
-      private changeDetectorRef: ChangeDetectorRef,
-      private router: Router,
-      private accountService: AccountService,
-      private formBuilder: FormBuilder,
-      private toastr: ToastrService,
-      private plantService: PlantService) {
-        this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
-          this.user = user;
-        });
-        this.AddAction = this.formBuilder.group({
-          userId: [null, Validators.required],
-          plantId: [null, Validators.required],
-          needId: [null, Validators.required],
-          dateActionDone : [null, Validators.required]
-        });
-      }
+    private changeDetectorRef: ChangeDetectorRef,
+    private router: Router,
+    private accountService: AccountService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService,
+    private plantService: PlantService
+  ) {
+    
+  }
 
   ngOnInit(): void {
     this.dashboardService.getTypes().subscribe((data: FrequencyType[]) => {
@@ -63,8 +78,6 @@ export class DashboardComponent implements OnInit {
     this.plantService.getPlants().subscribe((data: Plant[]) => {
       this.plants$.next(data);
     });
-    
-    
   }
 
   apply() {
@@ -76,29 +89,14 @@ export class DashboardComponent implements OnInit {
     this.changeDetectorRef.detectChanges();
   }
 
-  actionDone(plant: Plant , need: PlantNeed){
-    this.curDate=new Date();
-    this.AddAction.value['userId']=this.user.id;
-    this.AddAction.value['plantId']=plant.id;
-    this.AddAction.value['needId']=need.needId;
-    this.AddAction.value['dateActionDone']=this.curDate;
-    this.dashboardService.addPlant(this.AddAction.value).subscribe(
-      (response) => {
-        this.toastr.success(need.needName+" done!");
-      },
-      (error) => {
-        this.toastr.error(error);
-      }
-    );
-  }
-
-  needAction(need: PlantNeed){
+  needAction(need: PlantNeed) {
     var action = true;
-    this.dashboardService.needAction(need.needId,need.plantId,1).subscribe(response => {
-      console.log(response);
-      action = response;
-    })
+    this.dashboardService
+      .needAction(need.needId, need.plantId, 1)
+      .subscribe((response) => {
+        console.log(response);
+        action = response;
+      });
     return action;
   }
-
 }
